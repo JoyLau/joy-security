41571e9108efed24ef7061999c7750f8dcf6a2b5
JoyLau <2587038142@qq.com>
2019-09-04 11:40:11 +0800

    init

--- joy-security-app/main.js
+++ joy-security-app/main.js
@@ -1,30 +1,37 @@
 const {app, BrowserWindow} = require('electron');
 let win;
 let windowConfig = {
-    width:800,
-    height:600
+    width: 800,
+    height: 600
 };
-function createWindow(){
+
+function createWindow() {
+    // require('getmac').getMac(function(err, macAddress){
+    //     if (err)  throw err
+    //     console.log(macAddress)
+    // })
+
+    console.info(require('os').networkInterfaces())
     win = new BrowserWindow(windowConfig);
     win.loadURL(`file://${__dirname}/index.html`);
     //开启调试工具
     win.webContents.openDevTools();
-    win.on('close',() => {
+    win.on('close', () => {
         //回收BrowserWindow对象
         win = null;
-});
-    win.on('resize',() => {
+    });
+    win.on('resize', () => {
         win.reload();
-})
+    })
 }
 
-app.on('ready',createWindow);
-app.on('window-all-closed',() => {
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
     app.quit();
 });
 
-app.on('activate',() => {
-    if(win == null){
-    createWindow();
-}
+app.on('activate', () => {
+    if (win == null) {
+        createWindow();
+    }
 });
