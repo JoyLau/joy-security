63b57ad76b7da548ad8a3dcc3ef031244f1a5c56
JoyLau <2587038142@qq.com>
2019-09-19 18:04:45 +0800

    add require fs

--- joy-security-app/public/renderer.js
+++ joy-security-app/public/renderer.js
@@ -1,4 +1,4 @@
 global.electron = require('electron');
 global.electronLocalshortcut = require('electron-localshortcut');
-global.fs = require('fs-extra');
+global.fs = require('fs');
 global.cp = require('child_process');
